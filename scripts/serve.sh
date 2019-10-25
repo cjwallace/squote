5c54d0004b56f75e07ab00217b083d038f416632
Chris Wallace <chris@bitcollider.io>
2019-10-25 01:19:23 +0100

    Reduce bert server default workers to one.

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,1 +1,1 @@
-bert-serving-start -model_dir models/cased_L-12_H-768_A-12 -num_worker=4 -max_seq_len=NONE
+bert-serving-start -model_dir models/cased_L-12_H-768_A-12 -num_worker=1 -max_seq_len=NONE
