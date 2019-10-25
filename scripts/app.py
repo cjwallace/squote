faa8d24ca49b5691034cbf8424d369fc0ede9927
Chris Wallace <chris@bitcollider.io>
2019-10-25 11:55:16 +0100

    Clean up comments and add explanatory text.

--- scripts/app.py
+++ scripts/app.py
@@ -12,7 +12,10 @@ from bert_serving.client import BertClient
 st.title("Squote")
 st.subheader("Semantic quote search")
 
-"""Enter your text and get a questionably relevant quote :)"""
+"""
+Enter your text and get some questionably relevant,
+questionably famous quotes.
+"""
 
 @st.cache(allow_output_mutation=True)
 def load_quotes_and_embeddings():
@@ -28,6 +31,7 @@ quotes, embeddings = load_quotes_and_embeddings()
 
 bc = BertClient()
 
+# Create an index for fast vector search
 dim = embeddings.shape[1]
 index = faiss.IndexFlatL2(dim)
 index.add(embeddings)
@@ -37,16 +41,35 @@ text = st.text_area(
     "I have a dream."
 )
 
+# normalize embedding for cosine distance
 text_embedding = bc.encode([text])
 normalized_text_embedding = text_embedding / text_embedding.sum()
 
-d, i = index.search(normalized_text_embedding, 5)
+_, idx = index.search(normalized_text_embedding, 5)
 
-relevant_quotes = quotes.iloc[i.flatten()].QUOTE.values
-relevant_authors = quotes.iloc[i.flatten()].AUTHOR.values
+relevant_quotes = quotes.iloc[idx.flatten()].QUOTE.values
+relevant_authors = quotes.iloc[idx.flatten()].AUTHOR.values
 
-"""Here are five "relevant" quotes:"""
+st.subheader("Your quotes")
 
-for ix in range(5):
-    st.markdown('>'+relevant_quotes[ix])
-    st.text(relevant_authors[ix])
+for q in range(5):
+    st.markdown('>'+relevant_quotes[q])
+    st.text(relevant_authors[q])
+
+st.subheader("What is this?")
+
+st.markdown(
+    """
+    It's semantic search powered by:
+    - [bert-as-a-service](https://github.com/hanxiao/bert-as-service)
+        &larr; the math
+    - [faiss](https://github.com/facebookresearch/faiss)
+        &larr; the search
+    - [streamlit](https://github.com/streamlit/streamlit)
+        &larr; the interface
+
+    The repo is hosted on GitHub at [cjwallace/squote](https://github.com/cjwallace/squote).
+
+    Made by [chrjs](chrjs.io).
+    """
+)
